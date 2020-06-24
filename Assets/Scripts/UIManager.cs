ada15c455e36180e6c2a37b35ab991ab985d58a5
Alan <alanmagelssen@gmail.com>
2020-06-23 22:49:00 -0700

    update ui

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Text pointsText;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(pointsText);
+    }
+
+    public void updatePoints(int points)
+    {
+        pointsText.text = points.ToString();
+    }
+}
