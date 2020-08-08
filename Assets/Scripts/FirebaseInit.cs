36ba65e990b0f7c4852a581ef20155e6ffa1e432
Alan <alanmagelssen@gmail.com>
2020-08-07 17:32:27 -0700

    firebase database pulling in data from db

---
+++ Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using Firebase.Analytics;
+using UnityEngine;
+
+public class FirebaseInit : MonoBehaviour
+{
+    void Start()
+    {
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://april-785ab.firebaseio.com/");
+
+        // Get the root reference location of the database.
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        FirebaseDatabase.DefaultInstance
+        .GetReference("instrument")
+        .GetValueAsync().ContinueWith(task => {
+            if (task.IsFaulted)
+            {
+                // Handle the error...
+                Debug.Log("unable to retrieve database information");
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                // Do something with snapshot...
+                Debug.Log(snapshot);
+            }
+        });
+
+        //FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        //{
+        //    FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        //});
+    }
+}
